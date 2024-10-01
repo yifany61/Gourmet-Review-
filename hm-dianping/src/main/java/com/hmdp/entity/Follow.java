05e82573478fcbf1c1146f07d6444b186f582817
Yifan Yang <you@example.com>
2024-09-30 21:56:02 -0500

    follow and unfollow other user function

--- hm-dianping/src/main/java/com/hmdp/entity/Follow.java
+++ hm-dianping/src/main/java/com/hmdp/entity/Follow.java
@@ -47,5 +47,35 @@ public class Follow implements Serializable {
      */
     private LocalDateTime createTime;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getFollowUserId() {
+        return followUserId;
+    }
+
+    public void setFollowUserId(Long followUserId) {
+        this.followUserId = followUserId;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
 }
