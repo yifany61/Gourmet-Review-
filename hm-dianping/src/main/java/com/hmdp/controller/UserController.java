f6ececdfdc194cf122697ac0306b857fe8054343
Yifan Yang <you@example.com>
2024-10-07 16:32:13 -0500

    sign in function

--- hm-dianping/src/main/java/com/hmdp/controller/UserController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/UserController.java
@@ -92,4 +92,9 @@ public class UserController {
         UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
         return Result.ok(userDTO);
     }
+
+    @PostMapping("/sign")
+    public Result sign() {
+        return userService.sign();
+    }
 }
