0b01c8e4651b9cd42cffdac9da5c190f9d355330
Yifan Yang <you@example.com>
2024-10-07 16:43:41 -0500

    consecutive sign-in number function

--- hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -14,6 +14,7 @@ import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
 import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.BitFieldSubCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +24,7 @@ import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -118,6 +120,44 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         // 5. 写入Redis SETBIT key offset 1
         stringRedisTemplate.opsForValue().setBit(key, dayOfMonth -1, true);
         return Result.ok();
+    }
+
+    @Override
+    public Result signCount() {
+        // 1. 获取当前登录用户
+        long userId = UserHolder.getUser().getId();
+        // 2. 获取日期
+        LocalDateTime now = LocalDateTime.now();
+        // 3. 拼接key
+        String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+        String key = USER_SIGN_KEY + userId + keySuffix;
+        // 4. 今天是本月的第几天
+        int dayOfMonth = now.getDayOfMonth();
+        // 5. 获取本月截至今天位置的所有签到记录 -- 返回的是一个十进制的数字
+        List<Long> result = stringRedisTemplate.opsForValue().bitField(
+                key,
+                BitFieldSubCommands.create()
+                        .get(BitFieldSubCommands.BitFieldType.unsigned(dayOfMonth)).valueAt(0)
+        );
+        if (result == null || result.isEmpty())
+            return Result.ok(0);
+        Long num = result.get(0);
+        if (num == null || num == 0)
+            return Result.ok(0);
+        // 6. 循环遍历
+        int count = 0;
+        while (true) {
+            // 6.1 让这个数字与1做与运算，得到数字的最后一个bit位, 判断这个bit位是否为0
+            if ((num & 1) == 0) {
+                break; // 如果为0，说明未签到，结束
+            } else {
+                count++; // 如果不为0，说明已签到，计数器+1
+            }
+            // 把数字右移一位，抛弃最后一个bit位，继续下一个bit位
+            num >>>= 1;
+        }
+
+        return Result.ok(count);
     }
 
     private User createUserWithPhone(String phone) {
